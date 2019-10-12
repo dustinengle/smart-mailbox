25e9f7b9ebc787171a54882d64103761c855e915
Chris Barden <chrisbarden@pop-os.localdomain>
2019-10-12 13:56:37 -0700

    updated mailbox code

--- box/pins.cpp
+++ box/pins.cpp
@@ -2,10 +2,11 @@
 #include "config.h"
 #include "mailbox.h"
 
-char path[6] = {0};
-
-void get_path(uint16_t pin) {
-    sprintf(path + 1, "%u", pin);
+//char path[6] = {0};
+String path = "";
+String get_path(String pin) {
+    //sprintf(path + 1, "%u", pin);
+    return String("/")+pin;
 }
 
 int allow_init() {
@@ -14,35 +15,44 @@ int allow_init() {
     return E_OK;
 }
 
-int allow_del(uint16_t pin) {
-    get_path(pin);
+int allow_del(String pin) {
+    path = get_path(pin);
     if (DEBUG) {
         Serial.print("ALLOW: del pin=");
-        Serial.print(pin, DEC);
+        Serial.print(pin);
         Serial.print(", path=");
         Serial.println(path);
     }
-    return file_remove((const char *)path);
+    char temp[6];
+    strcpy(temp, path.c_str());
+    return file_remove(temp);
+    //return file_remove((const char *)path);
 }
 
-int allow_get(uint16_t pin) {
-    get_path(pin);
+int allow_get(String pin) {
+    path = get_path(pin);
     if (DEBUG) {
         Serial.print("ALLOW: get pin=");
-        Serial.print(pin, DEC);
+        Serial.print(pin);
         Serial.print(", path=");
         Serial.println(path);
     }
-    return file_exists((const char *)path);
+    char temp[6];
+    strcpy(temp, path.c_str());
+    return file_exists(temp);
+    //return file_exists((const char *)path);
 }
 
-int allow_set(uint16_t pin) {
-    get_path(pin);
+int allow_set(String pin) {
+    path = get_path(pin);
     if (DEBUG) {
         Serial.print("ALLOW: set pin=");
-        Serial.print(pin, DEC);
+        Serial.print(pin);
         Serial.print(", path=");
         Serial.println(path);
     }
-    return file_write(path, (unsigned char *)path, 2);
+    char temp[6];
+    strcpy(temp, path.c_str());
+    return file_write(temp,(unsigned char *)temp,2);
+    //return file_write(path, (unsigned char *)path, 2);
 }
