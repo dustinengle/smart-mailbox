972689bc2fc1dd9f52de8ab5bc135fecd7ae4e12
Dustin Engle <dustin.engle@gmail.com>
2019-10-24 10:51:12 -0700

    remove soft delete at field

--- api/pkg/model/account.go
+++ api/pkg/model/account.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Account struct {
-	CreatedAt time.Time  `gorm:"" json:"createdAt"`
-	DeletedAt *time.Time `gorm:"" json:"deletedAt,omitempty"`
-	Email     string     `gorm:"unique" json:"email"`
-	ID        uint       `gorm:"primary_key" json:"id,omitempty"`
-	Password  string     `gorm:"" json:"-"`
-	PublicKey string     `gorm:"unique" json:"publicKey,omitempty"`
-	Token     string     `gorm:"" json:"-"`
-	UpdatedAt time.Time  `gorm:"" json:"updatedAt,omitempty"`
+	CreatedAt time.Time `gorm:"" json:"createdAt"`
+	//DeletedAt *time.Time `gorm:"" json:"deletedAt,omitempty"`
+	Email     string    `gorm:"unique" json:"email"`
+	ID        uint      `gorm:"primary_key" json:"id,omitempty"`
+	Password  string    `gorm:"" json:"-"`
+	PublicKey string    `gorm:"unique" json:"publicKey,omitempty"`
+	Token     string    `gorm:"" json:"-"`
+	UpdatedAt time.Time `gorm:"" json:"updatedAt,omitempty"`
 }
